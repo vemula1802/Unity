58e55c6b1b5d3d3bf822a326dff4d7983bd26bbe
Rajashekar <51023788+vemula1802@users.noreply.github.com>
2020-04-03 09:52:45 -0500

    Update PlayerControllerX.cs

--- challenge1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ challenge1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
